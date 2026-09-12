c75d8f83863aa4d2d372d3716d895b2c1009e46b
Dust North Games <masonsandersbussiness@gmail.com>
2026-09-13 00:48:46 +0100

    delted

--- config.js
+++
@@ -1,5 +0,0 @@
-window.ONEEDUCATION_CONFIG = Object.freeze({
-  supabaseUrl: 'https://naxoplmtweovwaiwvexe.supabase.co',
-  supabaseKey: 'sb_publishable_sgSPQOw2SEtIvE1iLTh_oA_nLCD8CEk',
-  adminEmail: 'masonsandersbussiness@gmail.com'
-});
